c393705707783243a3e38aaad59ea297c764535d
chogan72 <chrishogan99@hotmail.com>
2021-09-17 11:50:17 -0500

    JS Variables

---
+++ main.js
@@ -0,0 +1,30 @@
+// Questions 1
+let myNumber;
+
+// Questions 2
+myNumber = 5;
+
+// Question 3
+let myOtherNumber = 5;
+
+// Question 4
+let myTotal = myNumber * myOtherNumber;
+
+// Question 5
+let myName = "Chris";
+let myCombo = String(myNumber + myOtherNumber);
+
+// Question 6
+let myOtherCombo = myNumber + myOtherNumber;
+
+// Question 7
+let myRemainder = myNumber / 5;
+
+// Question 8
+let i = 2;
+
+// Question 9
+let int = 4;
+
+// Question 10
+let myBoolean = false;
